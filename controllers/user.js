38af3fe128b904604389e9030c60ff8ebf06dd39
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-07-07 11:33:55 +0100

    Routes: new route endpoint added
    new controllers added

--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,42 @@ const transactions = require('../middlewares/mongooseTransaction');
 const bcrypt = require('bcryptjs');
 
 async function hashPassword(enteredPassword) {
-  return await bcrypt.hash(enteredPassword, bcrypt.genSalt(10));
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(enteredPassword, salt);
 }
+
+const getUser = async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const userDoc = await model.UserModel.findById(id);
+    const { password, ...rest } = userDoc._doc;
+    res.status(201).json({ ...rest });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const getUsers = async (req, res, next) => {
+  const query = req.query.new;
+  try {
+    const usersDoc = query ? await model.UserModel.find().sort('createdAt').limit(5) : await model.UserModel.find();
+    res.status(201).json(usersDoc);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const usersStats = async (req, res, next) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const userStats = await model.UserModel.aggregate([{ $match: { createdAt: { $gte: lastYear } } }, { $project: { $month: '$createdAt' } }, { $group: { _id: '$month', total: { $sum: 1 } } }]);
+
+    res.status(201).json(userStats);
+  } catch (error) {}
+};
 
 const updateUser = transactions(async (req, res, session) => {
   const {
@@ -12,20 +46,35 @@ const updateUser = transactions(async (req, res, session) => {
   } = req;
 
   if (req.body.password) {
-    const hashedPassword = await hashPassword(password);
-    req.body.password = hashedPassword;
+    req.body.password = await hashPassword(req.body.password);
   }
 
   try {
-    const newUserDocument = await model.UserModel.findByIdAndUpdate(id, { $set: req.body }, { new: true });
+    const updatedUserDocument = await model.UserModel.findByIdAndUpdate(id, { $set: req.body }, { new: true });
+    await updatedUserDocument.save({ session });
 
-    await newUserDocument.save({ session });
-    res.status(201).json(newUserDocument);
+    res.status(201).json(updatedUserDocument);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
+const deleteUser = async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    await model.UserModel.findByIdAndDelete(id);
+    res.status(201).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
+  getUser,
+  getUsers,
+  usersStats,
   updateUser,
+  deleteUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,12 @@ const controller = require('../controllers/index');
 const router = express.Router();
 const { auth, isAdmin } = require('../utils/auth');
 
-router.get('/');
+router.get('/', isAdmin, controller.userController.getUsers);
+router.get('/user/:id', isAdmin, controller.userController.getUser);
+router.get('/stats', isAdmin, controller.userController.usersStats);
 router.post('/register', controller.authController.newUser);
 router.post('/login', controller.authController.login);
 router.put('/:id', auth, controller.userController.updateUser);
+router.delete('/:id', auth, controller.userController.deleteUser);
 
 module.exports = router;
