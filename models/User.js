cd4e8eb69e1ca9ac082d2ab1d5ddca3d00ea9b77
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-07-07 05:02:10 +0100

    Model: unique property remove from username

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const { Schema, model } = mongoose;
 
 const UserSchema = new Schema(
   {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
