c41a9a905184f220c288a2e78c823b82a1b377e0
Yunus Abbas Opeyemi <abbasopeyemi148@gmail.com>
2024-11-14 12:56:58 +0100

    product category modified

--- controllers/ecommerce/category.controller.js
+++ controllers/ecommerce/category.controller.js
@@ -1,35 +1,70 @@
-const model = require("../../models/index");
-const { asyncHandler } = require("../../utils/asyncHandler");
-const { ApiResponse } = require("../../utils/api.response");
-const { ApiError } = require("../../utils/api.error");
-const { StatusCodes } = require("http-status-codes");
+const model = require('../../models/index');
+const { asyncHandler } = require('../../utils/asyncHandler');
+const { ApiResponse } = require('../../utils/api.response');
+const { ApiError } = require('../../utils/api.error');
+const { StatusCodes } = require('http-status-codes');
 
 const createCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
 
   const existingCategory = await model.CategoryModel.findOne({ name });
 
-  if (existingCategory) throw new ApiError(StatusCodes.CONFLICT, "category already exist", []);
+  if (existingCategory) throw new ApiError(StatusCodes.CONFLICT, 'category already exist', []);
 
   const category = await model.CategoryModel.create({
     name,
     owner: req.user._id,
   });
 
-  return new ApiResponse(StatusCodes.CREATED, "user category created successfully", { category });
+  return new ApiResponse(StatusCodes.CREATED, 'user category created successfully', { category });
 });
 
 const getAllCategory = asyncHandler(async (req, res) => {
-  const categories = await model.CategoryModel.find({}).sgit stelect("name _id");
+  const categories = await model.CategoryModel.find({}).select('name _id');
 
-  return new ApiResponse(StatusCodes.OK, "catergories fetched successfully", { categories });
+  return new ApiResponse(StatusCodes.OK, 'catergories fetched successfully', { categories });
 });
 
-const updateCategory = asyncHandler(async (req, res) => {});
+const updateCategory = asyncHandler(async (req, res) => {
+  const { categoryId } = req.params;
+  const { name } = req.body;
 
-const getCategoryById = asyncHandler(async (req, res) => {});
+  const category = await model.CategoryModel.findByIdAndUpdate(
+    categoryId,
+    {
+      $set: {
+        name,
+      },
+    },
+    { new: true }
+  );
+  if (!category) throw new ApiError(StatusCodes.NOT_FOUND, 'category not found', []);
 
-const deleteCategory = asyncHandler(async (req, res) => {});
+  await category.save();
+
+  return new ApiResponse(StatusCodes.OK, 'catergory updated successfully', { category });
+});
+
+const getCategoryById = asyncHandler(async (req, res) => {
+  const { categoryId } = req.params;
+
+  const category = await model.CategoryModel.findById(category);
+
+  if (!category) throw new ApiError(StatusCodes.NOT_FOUND, 'category not found', []);
+
+  return new ApiResponse(StatusCodes.OK, 'catergories fetched successfully', { category });
+});
+
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { categoryId } = req.params;
+
+  const category = await model.CategoryModel.findByIdAndDelete(categoryId);
+  if (!category) throw new ApiError(StatusCodes.NOT_FOUND, 'category not found', []);
+
+  await category.save();
+
+  return new ApiResponse(StatusCodes.OK, 'catergory deleted successfully');
+});
 
 module.exports = {
   createCategory,
--- routes/ecommerce/category.routes.js
+++ routes/ecommerce/category.routes.js
@@ -1,16 +1,30 @@
-const express = require("express");
-const controllers = require("../../controllers/index");
-const { verifyJWT, checkPermissions } = require("../../middlewares/auth.middleware");
-const { RoleEnums } = require("../../constants");
+const express = require('express');
+const controllers = require('../../controllers/index');
+const { verifyJWT, checkPermissions } = require('../../middlewares/auth.middleware');
+const { RoleEnums } = require('../../constants');
 const router = express.Router();
 
 router
-  .route("/")
+  .route('/')
   .get(verifyJWT, controllers.categoryController.getAllCategory)
   .post(
     verifyJWT,
     checkPermissions(RoleEnums.ADMIN),
-    controllers.categoryController.createCategory,
+    controllers.categoryController.createCategory
+  );
+
+router
+  .route('/:categoryId')
+  .get(verifyJWT, checkPermissions(RoleEnums.ADMIN), controllers.categoryController.getCategoryById)
+  .patch(
+    verifyJWT,
+    checkPermissions(RoleEnums.ADMIN),
+    controllers.categoryController.updateCategory
+  )
+  .delete(
+    verifyJWT,
+    checkPermissions(RoleEnums.ADMIN),
+    controllers.categoryController.deleteCategory
   );
 
 module.exports = router;
