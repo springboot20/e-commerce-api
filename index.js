1bf94b29202174d481e3c13fe276823fd21a474c
springboot20 <abbasopeyemi148@gmail.com>
2024-11-28 04:43:00 +0100

    modified

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.use("/api/v1/addresses", routers.addressesRouter);
 app.use("/api/v1/carts", routers.cartsRouter);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
