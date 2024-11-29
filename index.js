084116aafcd763b05b862929b69b81d508d80cf0
springboot20 <abbasopeyemi148@gmail.com>
2024-11-29 12:04:18 +0100

    cors middleware modified

--- index.js
+++ index.js
@@ -16,7 +16,24 @@ const PORT = process.env.PORT ?? 5000;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = ["https://cv-ecommerce-project.vercel.app/"];
+
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // Allow cookies and credentials
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
