795120e71da24308605c3ec6017f0590d70c50da
springboot20 <abbasopeyemi148@gmail.com>
2024-11-30 00:12:46 +0100

    modified

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    allowedHeaders: process.env.CORS_ORIGIN,
+    allowedHeaders: "*",
     credentials: true,
   }),
 );
