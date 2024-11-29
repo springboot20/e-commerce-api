ba17e19f0418579e04519822815935bdb4dd0ba7
springboot20 <abbasopeyemi148@gmail.com>
2024-11-29 11:40:29 +0100

    modified

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/api/v1/carts", routers.cartsRouter);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Authorization");
   next();
 });
 
