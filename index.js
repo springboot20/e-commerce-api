281d99c00ac7bb73efe5ef5d8b1275be049d4a10
springboot20 <abbasopeyemi148@gmail.com>
2024-11-29 11:34:27 +0100

    modified

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const PORT = process.env.PORT ?? 5000;
 
 app.use(express.json());
 
-app.use(
-  cors(),
-);
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -34,7 +32,7 @@ app.use("/api/v1/carts", routers.cartsRouter);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
