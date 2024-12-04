93d5dd250324b76604aa8b79bf4e241927bc53fa
springboot20 <abbasopeyemi148@gmail.com>
2024-12-04 04:29:28 +0100

    allowed PATCH request method

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/api/v1/statistics", routers.statisticsRouter);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
   next();
