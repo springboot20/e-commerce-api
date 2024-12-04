09b9d4642ac8c5d0e204aacc31ffb51f0eb7a0fc
springboot20 <abbasopeyemi148@gmail.com>
2024-12-04 04:45:51 +0100

    cors updated

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
   next();
