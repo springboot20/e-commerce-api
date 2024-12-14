95f0eb1906e0a2011dd5e96cf5571fb04b7e3806
springboot20 <abbasopeyemi148@gmail.com>
2024-12-14 19:35:13 +0100

    modified validation

--- validation/auth/user.validation.js
+++ validation/auth/user.validation.js
@@ -12,12 +12,7 @@ const registerValidator = () => [
     .isEmail()
     .normalizeEmail()
     .withMessage("must be a valid email format"),
-  body("phone_number")
-    .trim()
-    .notEmpty()
-    .withMessage("phone number is required")
-    .isMobilePhone("any")
-    .withMessage("must be a valid phone number format"),
+  body("phone_number").trim().notEmpty().withMessage("phone number is required"),
   body("role").optional().trim().isIn(AvailableRoles).withMessage("Invalid user role"),
 ];
 
