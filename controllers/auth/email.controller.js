0c8c79306077935dd1f50cb3441c2b427476f68e
springboot20 <abbasopeyemi148@gmail.com>
2024-12-19 22:24:24 +0100

    implemented new logic for sending email for forgot password and email verification

--- controllers/auth/email.controller.js
+++ controllers/auth/email.controller.js
@@ -1,10 +1,10 @@
-const { asyncHandler } = require("../../utils/asyncHandler");
-const { ApiResponse } = require("../../utils/api.response");
-const { ApiError } = require("../../utils/api.error");
-const { StatusCodes } = require("http-status-codes");
-const model = require("../../models/index");
-const { sendMail } = require("../../service/email.service");
-const bcrypt = require("bcryptjs");
+const { asyncHandler } = require('../../utils/asyncHandler');
+const { ApiResponse } = require('../../utils/api.response');
+const { ApiError } = require('../../utils/api.error');
+const { StatusCodes } = require('http-status-codes');
+const model = require('../../models/index');
+const { sendMail } = require('../../service/email.service');
+const bcrypt = require('bcryptjs');
 
 const emailVerification = asyncHandler(
   /**
@@ -17,7 +17,7 @@ const emailVerification = asyncHandler(
   async (req, res) => {
     const { token, email } = req.body;
 
-    if (!token) throw new ApiError(StatusCodes.UNAUTHORIZED, "verification token missing");
+    if (!token) throw new ApiError(StatusCodes.UNAUTHORIZED, 'verification token missing');
 
     const user = await model.UserModel.findOne({
       email,
@@ -27,12 +27,12 @@ const emailVerification = asyncHandler(
     if (!user)
       throw new ApiError(
         StatusCodes.UNAUTHORIZED,
-        "unable to verify user, token invalid or expired",
+        'unable to verify user, token invalid or expired'
       );
 
     const validToken = await bcrypt.compare(token, user?.emailVerificationToken);
 
-    if (!validToken) throw new ApiError(StatusCodes.UNAUTHORIZED, "Invalid email token provided");
+    if (!validToken) throw new ApiError(StatusCodes.UNAUTHORIZED, 'Invalid email token provided');
 
     user.emailVerificationToken = undefined;
     user.emailVerificationTokenExpiry = undefined;
@@ -40,10 +40,10 @@ const emailVerification = asyncHandler(
 
     await user.save({ validateBeforeSave: false });
 
-    return new ApiResponse(StatusCodes.OK, "user email verified successfully", {
+    return new ApiResponse(StatusCodes.OK, 'user email verified successfully', {
       isEmailVerified: true,
     });
-  },
+  }
 );
 
 const forgotPassword = asyncHandler(
@@ -58,48 +58,42 @@ const forgotPassword = asyncHandler(
     const user = await model.UserModel.findOne({ email });
 
     if (!user) {
-      throw new ApiError(StatusCodes.NOT_FOUND, "user does not exists", []);
+      throw new ApiError(StatusCodes.NOT_FOUND, 'user does not exists', []);
     }
 
     const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryTokens();
 
     user.forgotPasswordToken = hashedToken;
-    user.forgotPasswordExpiry = tokenExpiry;
+    user.forgotPasswordTokenExpiry = tokenExpiry;
 
     await user.save({ validateBeforeSave: false });
 
     await sendMail(
       user.email,
-      "Password reset",
+      'Password reset',
       { resetCode: unHashedToken, username: user?.username },
-      "reset",
+      'reset'
     );
 
-    return new ApiResponse(StatusCodes.OK, "password reset link sent successfully");
-  },
+    return new ApiResponse(StatusCodes.OK, 'password reset link sent successfully');
+  }
 );
 
-const resendEmailVerification = asyncHandler(
+const resendEmailVerificationExistingUser = asyncHandler(
   /**
    * @param {import("express").Request} req
    * @param {import("express").Response} res
    */
 
   async (req, res) => {
-    const user = await model.UserModel.findOne({
-      $or: [{ email: req.body.email }, { _id: req.user._id }],
-    });
+    const user = await model.UserModel.findOne({ _id: req.user._id });
 
     if (!user) {
-      throw new ApiError(StatusCodes.NOT_FOUND, "user does not exists", []);
+      throw new ApiError(StatusCodes.NOT_FOUND, 'user does not exists', []);
     }
 
     if (user.isEmailVerified) {
-      throw new CustomErrors.Conflict(
-        StatusCodes.CONFLICT,
-        "user email has already been verified",
-        [],
-      );
+      throw new ApiError(StatusCodes.CONFLICT, 'user email has already been verified', []);
     }
 
     const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryTokens();
@@ -111,19 +105,55 @@ const resendEmailVerification = asyncHandler(
 
     await sendMail(
       user?.email,
-      "Email verification",
+      'Email verification',
       { username: user?.username, verificationCode: unHashedToken },
-      "email",
+      'email'
     );
 
     await user.save({ validateBeforeSave: false });
 
-    return new ApiResponse(StatusCodes.OK, "Email verification resend");
-  },
+    return new ApiResponse(StatusCodes.OK, 'Email verification resent');
+  }
+);
+
+const resendEmailVerificationNewUser = asyncHandler(
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   */
+
+  async (req, res) => {
+    const { email } = req.body;
+
+    const user = await model.UserModel.findOne({ email });
+
+    if (!user) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'user does not exists', []);
+    }
+
+    const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryTokens();
+
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationTokenExpiry = tokenExpiry;
+
+    await user.save({ validateBeforeSave: false });
+
+    await sendMail(
+      user?.email,
+      'Email verification',
+      { username: user?.username, verificationCode: unHashedToken },
+      'email'
+    );
+
+    await user.save({ validateBeforeSave: false });
+
+    return new ApiResponse(StatusCodes.OK, 'Email verification resent');
+  }
 );
 
 module.exports = {
   emailVerification,
   forgotPassword,
-  resendEmailVerification,
+  resendEmailVerificationExistingUser,
+  resendEmailVerificationNewUser,
 };
