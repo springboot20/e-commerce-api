e32c73f6fbbe406deaa077fb7e302bd29a1c59f8
springboot20 <abbasopeyemi148@gmail.com>
2025-01-14 16:46:34 +0100

    updated database connection

--- connection/connection.js
+++ connection/connection.js
@@ -2,11 +2,22 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: process.env.DBNAME,
-      user: process.env.USER,
-      pass: process.env.PASS,
-    });
+    const connectionString =
+      process.env.NODE_ENV === "production"
+        ? process.env.MONGODB_URI
+        : process.env.MONGODB_URI_LOCAL;
+
+    const connectData =
+      process.env.NODE_ENV === "production"
+        ? {
+            dbName: process.env.DBNAME,
+            user: process.env.USER,
+            pass: process.env.PASS,
+          }
+        : {
+            dbName: process.env.DBNAME,
+          };
+    const connectionInstance = await mongoose.connect(connectionString, connectData);
 
     console.log(`\n☘️  MongoDB connected successfully: ${connectionInstance.connection.host}\n`);
   } catch (error) {
