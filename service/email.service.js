c7d9c837498eff9fed87d986fd8d79adfb808c13
springboot20 <abbasopeyemi148@gmail.com>
2025-01-28 07:16:18 +0100

    updated email services

--- service/email.service.js
+++ service/email.service.js
@@ -1,42 +1,76 @@
-const { google } = require("googleapis");
-const nodemailer = require("nodemailer");
-const { StatusCodes } = require("http-status-codes");
-const { ApiError } = require("../utils/api.error.js");
-const expressHandlebars = require("nodemailer-express-handlebars");
-const path = require("path");
+const { google } = require('googleapis');
+const nodemailer = require('nodemailer');
+const { StatusCodes } = require('http-status-codes');
+const { ApiError } = require('../utils/api.error.js');
+const expressHandlebars = require('nodemailer-express-handlebars');
+const path = require('path');
+const fs = require('fs');
 
 const OAuth2 = google.auth.OAuth2;
 
 const isTokenExpired = (token) => {
-  let currentTimestamps = Math.floor(Date.now() / 10000);
-  return currentTimestamps > token.expire_date;
+  let currentTimestamps = Date.now();
+  return currentTimestamps >= token.expiry_date;
 };
 
-const refreshAccessToken = async (oauth) => {
+const saveTokens = (tokens) => {
+  const token_data = {
+    access_token: tokens?.access_token,
+    refresh_token: tokens?.refresh_token || process.env.REFRESH_TOKEN,
+    expiry_date: tokens?.expiry_date,
+  };
+
+  fs.writeSync('tokens.json', JSON.stringify(token_data));
+};
+
+const loadTokens = () => {
+  if (fs.existsSync('tokens.json')) {
+    const token_data = fs.readFileSync('tokens.json');
+    return JSON.parse(token_data);
+  }
+  return null;
+};
+
+const refreshAccessToken = async (refreshToken, clientId, clientSecret) => {
+  const OAuth2Client = new OAuth2(
+    clientId,
+    clientSecret,
+    'https://developers.google.com/oauthplayground'
+  );
+
+  OAuth2Client.setCredentials({
+    refresh_token: refreshToken,
+  });
+
   try {
-    const { tokens } = await oauth.refreshToken(oauth.credentials.refresh_token);
-    oauth.setCredential(tokens);
+    const { tokens } = await OAuth2Client.refreshAccessToken();
+    OAuth2.setCredential(tokens);
+
+    saveTokens(tokens);
+
     return tokens.access_token;
   } catch (error) {
     throw new ApiError(
       StatusCodes.UNAUTHORIZED,
-      `Error ocurred while refreshing access token: ${error}`,
+      `Error ocurred while refreshing access token: ${error}`
     );
   }
 };
 
 const createTransporter = async () => {
+  const tokens = loadTokens();
+
   const OAuth2Client = new OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground",
+    'https://developers.google.com/oauthplayground'
   );
 
   OAuth2Client.setCredentials({
-    access_token: process.env.ACCESS_TOKEN,
-    refresh_token: process.env.REFRESH_TOKEN,
-    token_type: "Bearer",
-    expiry_date: process.env.EXPIRY_DATE,
+    access_token: tokens ? tokens?.access_token : process.env.ACCESS_TOKEN,
+    refresh_token: tokens ? tokens?.refresh_token : process.env.REFRESH_TOKEN,
+    token_type: 'Bearer',
+    expiry_date: tokens ? tokens?.expiry_date : process.env.EXPIRY_DATE,
   });
 
   OAuth2Client.generateAuthUrl({
@@ -45,31 +79,23 @@ const createTransporter = async () => {
   });
 
   if (isTokenExpired(OAuth2Client.credentials)) {
-    try {
-      const refreshedAccessToken = await refreshAccessToken(OAuth2Client);
+    console.log('Access token expired Refreshing...');
 
-      OAuth2Client.setCredentials({
-        access_token: refreshedAccessToken,
-        refresh_token: process.env.REFRESH_TOKEN,
-        scope: process.env.SCOPES,
-        token_type: process.env.TOKEN_TYPE,
-        expiry_date: OAuth2Client.credentials.expiry_date,
-      });
-      
-    } catch (error) {
-      console.error("Failed to refresh access token");
-      return null;
-    }
+    await refreshAccessToken(
+      OAuth2Client.credentials.refresh_token,
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET
+    );
   }
 
   return nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
-      type: "OAuth2",
+      type: 'OAuth2',
       user: process.env.EMAIL,
       pass: process.env.EMAIL_HOST_PASSWORD,
       accessToken: OAuth2Client.credentials.access_token,
-      refreshToken: process.env.REFRESH_TOKEN,
+      refreshToken: OAuth2Client.credentials.refresh_token,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       pass: process.env.EMAIL_HOST_PASSWORD,
@@ -85,17 +111,17 @@ const sendMail = async (email, subject, payload, template) => {
     const transporter = await createTransporter();
 
     transporter.use(
-      "compile",
+      'compile',
       expressHandlebars({
         viewEngine: {
-          extName: ".hbs",
-          partialsDir: path.resolve(__dirname, "../views/partials/"),
-          layoutsDir: path.resolve(__dirname, "../views/layouts/"),
-          defaultLayout: "layout",
+          extName: '.hbs',
+          partialsDir: path.resolve(__dirname, '../views/partials/'),
+          layoutsDir: path.resolve(__dirname, '../views/layouts/'),
+          defaultLayout: 'layout',
         },
-        extName: ".hbs",
-        viewPath: path.resolve(__dirname, "../views/partials/"),
-      }),
+        extName: '.hbs',
+        viewPath: path.resolve(__dirname, '../views/partials/'),
+      })
     );
 
     const options = () => {
@@ -109,7 +135,7 @@ const sendMail = async (email, subject, payload, template) => {
     };
 
     const info = await transporter.sendMail(options());
-    console.log("Message Id : %s" + info.messageId);
+    console.log('Message Id : %s' + info.messageId);
   } catch (error) {
     throw error;
   }
